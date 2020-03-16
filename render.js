acbcebebf04de0e870e83108fdcb45186f47e83d
Divy Srivastava <dj.srivastava23@gmail.com>
2020-03-16 21:00:43 +0530

    Update render.js

--- render.js
+++ render.js
@@ -11,7 +11,7 @@ for(var i=0;i<window.data.length;i++) {
 var temp = `<br>
 <div class="d-flex flex-column flex-md-row flex-items-center flex-md-items-center">
   <div class="col-12 col-md-10 d-flex flex-column flex-justify-center flex-items-center flex-md-items-start pl-md-4">
-    <h1 class="text-normal lh-condensed">March 15</h1>
+    <h1 class="text-normal lh-condensed">DATE</h1>
     <p class="h4 text-gray text-normal mb-2">Sunday</p>
   </div>
 </div>`
