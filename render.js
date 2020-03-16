086c1bf1fd2978db9a33f99f61a9f2f6fa14a741
Divy Srivastava <dj.srivastava23@gmail.com>
2020-03-16 22:09:46 +0530

    Update render.js

--- render.js
+++ render.js
@@ -108,7 +108,7 @@ var temp = `<br>
     <h1 class="text-normal lh-condensed">DATE</h1>
     <p class="h4 text-gray text-normal mb-2">DAY</p>
   </div>
-   DESCRIPTION
+   <div class=container-md>DESCRIPTION</div>
 </div>`
 
 for(var i=0;i<window.data.length;i++) {
